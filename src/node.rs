0f5181236bee326394b1eadf294e4ec5dbb97fe6
iTranscend <llordbanks500@gmail.com>
2025-05-12 23:58:51 +0100

    feat: track streams for persistent connections

--- src/node.rs
+++ src/node.rs
@@ -1,10 +1,18 @@
-use std::{collections::HashSet, net::SocketAddr, sync::Arc, time::Duration};
+use std::{
+    collections::{HashMap, HashSet},
+    net::SocketAddr,
+    sync::Arc,
+    time::Duration,
+};
 
-use log::{error, info};
+use log::info;
 use tokio::{
     io::{AsyncReadExt, AsyncWriteExt},
     net::{TcpListener, TcpStream},
-    sync::RwLock,
+    sync::{
+        mpsc::{self, Receiver, Sender},
+        Mutex, RwLock,
+    },
 };
 
 use crate::message::{ConnectionInfo, Message};
@@ -19,7 +27,7 @@ pub struct Node {
     listener: TcpListener,
     bootstrap_node: Option<SocketAddr>,
     // Current active peers
-    active_peers: Arc<RwLock<HashSet<SocketAddr>>>,
+    connections: Arc<RwLock<HashMap<SocketAddr, Arc<Mutex<TcpStream>>>>>,
     // Peers we know about
     known_peers: Arc<RwLock<HashSet<SocketAddr>>>,
     config: NodeConfig,
@@ -36,7 +44,7 @@ impl Node {
             addr,
             listener,
             bootstrap_node,
-            active_peers: Arc::new(RwLock::new(HashSet::new())),
+            connections: Arc::new(RwLock::new(HashMap::new())),
             known_peers: Arc::new(RwLock::new(HashSet::new())),
             config,
         }))
@@ -51,11 +59,11 @@ impl Node {
         info!("Node listening on {}", self.addr);
         // Spawns a new task for each incoming connection
         loop {
-            let (socket_stream, addr) = self.listener.accept().await?;
+            let (socket_stream, _) = self.listener.accept().await?;
 
             let this = self.clone();
             tokio::spawn(async move {
-                this.handle_peer_connection(socket_stream, addr).await;
+                let _ = this.handle_peer_connection(socket_stream).await;
             });
         }
     }
@@ -78,55 +86,105 @@ impl Node {
         Ok(())
     }
 
-    async fn handle_peer_connection(
-        self: Arc<Self>,
-        mut socket_stream: TcpStream,
-        addr: SocketAddr,
-    ) {
-        let mut buffer = [0; 1024];
+    async fn handle_peer_connection(self: Arc<Self>, stream: TcpStream) -> anyhow::Result<()> {
+        // create channel for sending msgs between peer tasks
+        let (tx, mut rx): (
+            Sender<(Message, Arc<Mutex<TcpStream>>)>,
+            Receiver<(Message, Arc<Mutex<TcpStream>>)>,
+        ) = mpsc::channel(16);
+
+        let stream = Arc::new(Mutex::new(stream));
 
         loop {
-            let n = match socket_stream.read(&mut buffer).await {
-                Ok(n) if n == 0 => break,
-                Ok(n) => n,
-                Err(err) => {
-                    error!("Error reading from peer {}: {}", addr, err);
-                    break;
+            // TODO: use unsafe for TcpStream handling between sender and receiver tasks since we know only one of the tasks will execute
+            let tx = tx.clone();
+            let task = async {
+                let mut buffer = [0; 1024];
+
+                let n = stream.lock().await.read(&mut buffer).await?;
+
+                if n == 0 {
+                    return Ok::<Option<(Message, usize)>, anyhow::Error>(None);
                 }
+
+                let decoded_slice: (Message, usize) =
+                    bincode::decode_from_slice(&buffer[..n], bincode::config::standard())?;
+                Ok(Some(decoded_slice))
             };
 
-            let decoded_slice: Message =
-                match bincode::decode_from_slice(&buffer[..n], bincode::config::standard()) {
-                    Ok((message, _)) => message,
-                    Err(err) => {
-                        error!(
-                            "Error decoding message from peer when handling peer connection: {}",
-                            err
-                        );
-                        break;
+            tokio::select! {
+                res = task => {
+                    let res = res?;
+                    if let Some(res) = res {
+                        let _ = self.handle_peer_message(stream.clone(), res, tx).await;
                     }
-                };
-
-            let message = match decoded_slice {
-                Message::ConnectToPeer(connection_info) => connection_info,
-            };
-
-            info!("Received {} bytes from {}: {:#?}\n", n, addr, message);
-
-            let peer_listen_addr = message.listen_addr;
-
-            // add to active peers
-            self.active_peers.write().await.insert(peer_listen_addr);
-
-            // add to know_peers if not already there
-            let mut known_peers = self.known_peers.write().await;
-            if !known_peers.contains(&peer_listen_addr) {
-                known_peers.insert(peer_listen_addr);
+                }
+                res = rx.recv() => {
+                    if let Some((message, stream)) = res {
+                        let bytes = Self::serialize(message).await?;
+                        stream.lock().await.write(&bytes).await?;
+                    }
+                }
             }
-
-            // TODO:Respond to peer for peer discovery
         }
 
-        self.active_peers.write().await.remove(&addr);
+        // self.connections.write().await.remove(&addr);
+        // Ok(())
+    }
+
+    async fn handle_peer_message(
+        &self,
+        stream: Arc<Mutex<TcpStream>>,
+        message: (Message, usize),
+        tx: Sender<(Message, Arc<Mutex<TcpStream>>)>,
+    ) -> anyhow::Result<()> {
+        let bytes = message.1;
+        let message = match message.0 {
+            Message::ConnectToPeer(connection_info) => connection_info,
+        };
+
+        info!("Received {} bytes {:#?}\n", bytes, message);
+
+        let peer_listen_addr = message.listen_addr;
+
+        // add to active connections map
+        self.connections
+            .write()
+            .await
+            .insert(peer_listen_addr, stream.clone());
+
+        // add to known_peers if not already there
+        let mut known_peers = self.known_peers.write().await;
+        if !known_peers.contains(&peer_listen_addr) {
+            known_peers.insert(peer_listen_addr);
+        }
+
+        info!("Respond to peer");
+
+        // TODO:Respond to peer for peer discovery
+        let response = Message::ConnectToPeer(ConnectionInfo {
+            listen_addr: self.addr,
+            known_peers: self.known_peers.read().await.clone(),
+            message: Some(format!("Sup peer")),
+        });
+
+        self.send_message(tx, response, stream).await;
+
+        Ok(())
+    }
+
+    async fn send_message(
+        &self,
+        tx: Sender<(Message, Arc<Mutex<TcpStream>>)>,
+        message: Message,
+        stream: Arc<Mutex<TcpStream>>,
+    ) {
+        let _ = tx.send((message, stream)).await;
+    }
+
+    async fn serialize(message: Message) -> anyhow::Result<Vec<u8>> {
+        let config = bincode::config::standard();
+        let bytes = bincode::encode_to_vec(message, config)?;
+        Ok(bytes)
     }
 }
